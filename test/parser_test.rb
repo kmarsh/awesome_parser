9f04ffc71943d961f4003d6bf01baac7317e9653
Kevin Marsh <kevin.marsh@gmail.com>
2015-06-20 11:30:24 -0400

    Add go example

--- test/parser_test.rb
+++ test/parser_test.rb
@@ -16,4 +16,18 @@ class UrlTest < Minitest::Test
       line: "* [Griddler](https://github.com/thoughtbot/griddler) - Simplify receiving email in Rails\n"
     }
   end
+
+  def test_items_with_go
+    text = File.open("test/fixtures/avelino/awesome-go/README.md").read
+    parser = AwesomeParser::Parser.new(text)
+    parser.parse!
+
+    assert_includes parser.items, {
+      url: "https://github.com/blevesearch/bleve",
+      title: "bleve",
+      description: "A modern text indexing library for go.",
+      heading: nil,
+      line: "    * [bleve](https://github.com/blevesearch/bleve) - A modern text indexing library for go.\n"
+    }
+  end
 end
